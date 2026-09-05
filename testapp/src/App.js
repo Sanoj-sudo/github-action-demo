08101ec0e9b82f68818086deb6df7941d3715810
sanoj <sanoj@sanojs-MacBook-Pro.local>
2026-09-05 16:41:48 +0530

    added code for cicd

---
+++ testapp/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is a demo application for GitHub Actions workflow. You can edit the code and push it to your repository to see the workflow in action.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
